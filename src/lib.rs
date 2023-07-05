7f67bda5989ae32ce2ee42344aae7f4ffdb9a5f4
gsycl2004 <1544189298@qq.com>
2023-07-05 22:55:20 +0800

    优化代码结构

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{ffi::{c_int, c_char, c_ulonglong}, slice};
+use std::{ffi::{c_char, c_ulonglong, c_int}, mem};
 
 
 
@@ -21,11 +21,11 @@ mod tests {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn ncm2mp3(src:*mut c_char,src_length:c_ulonglong,dst:*mut c_char,dst_length:c_ulonglong){
-    
-    let src = Vec::from_raw_parts(src as *mut u8, src_length as usize, src_length as usize);
-    let dst = Vec::from_raw_parts(dst as *mut u8, dst_length as usize, dst_length as usize);
-    let src = String::from_utf8(src).unwrap();
-    let dst =String::from_utf8(dst).unwrap();
+pub unsafe extern "C" fn ncm2mp3(src:*mut c_char,src_length:c_ulonglong,dst:*mut c_char,dst_length:c_ulonglong) -> c_int {
+    let src = String::from_raw_parts(src as *mut u8, src_length as usize, src_length as usize);
+    let dst =String::from_raw_parts(dst as *mut u8, dst_length as usize, dst_length as usize);
     convert::ncm2mp3(src.clone(), dst.clone());
+    mem::forget(src);
+    mem::forget(dst);
+    1
 }
